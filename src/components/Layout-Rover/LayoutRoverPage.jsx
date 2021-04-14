953385cd60e02afa438465efb499aca1de9f7309
SebastienFrancois <sebastien.devlopweb@gmail.com>
2021-04-14 14:44:21 +0200

    initial structure of a page rover

---
+++ src/components/Layout-Rover/LayoutRoverPage.jsx
@@ -0,0 +1,15 @@
+import Navigation from '../Navigation/Navigation';
+import Footer from '../Footer/Footer';
+
+const LayoutRoverPage = () => (
+  <div className="rover-page-">
+    <Navigation />
+    {/* <Hero />
+    <Description />
+    <PictureSlider />
+    <PicturesGallery /> */}
+    <Footer />
+  </div>
+);
+
+export default LayoutRoverPage;

808e5b688ace4498df97a7b0b5bc72c772baa7e2
SebastienFrancois <sebastien.devlopweb@gmail.com>
2021-04-21 10:28:02 +0200

    Utils/Theme

---
+++ src/components/Navigation/components/Utils/Theme.jsx
@@ -0,0 +1,16 @@
+import { createMuiTheme } from '@material-ui/core';
+
+const Theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: '#fff',
+    },
+    secondary: {
+      main: '#941b0c',
+      dark: '#bc3908',
+      light: '#621708',
+    },
+    // type: "dark"
+  },
+});
+export default Theme;

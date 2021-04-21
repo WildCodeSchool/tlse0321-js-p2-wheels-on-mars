66d3450771b47e641face8fbc2911c15570b2070
Nicolas-concina <nicoconcina@gmail.com>
2021-04-21 11:03:24 +0200

    RoadMap update

--- src/components/RoadMap/ImageSlider.js
+++ src/components/RoadMap/ImageSlider.js
@@ -1,38 +1,33 @@
-import React from 'react'
-import React, {useState} from "./SliderData";
-import { SliderData } from './SliderData';
-import {FaArrowAltCircleRight, FaArrowAltCircleLeft } from "react-icons/fa"
-const ImageSlider = ({slide}) => {
-const [current, setCurrent]  =useState(0) 
-const lenght = slide.lenght ;
+import { FaArrowAltCircleRight, FaArrowAltCircleLeft } from 'react-icons/fa';
+import React, { useState, SliderData } from './SliderData';
 
-const nextSlide =()=>{
-    setCurrent(current === lenght -1 ? 0 : current + 1);
-};
+const ImageSlider = ({ slide }) => {
+  const [current, setCurrent] = useState(0);
+  const { lenght } = slide;
 
-const prevSlide =()=>{
-    setCurrent(current === 0 ? length -1 : current - 1);
-};
+  const nextSlide = () => {
+    setCurrent(current === lenght - 1 ? 0 : current + 1);
+  };
 
-if(!Array.isArray(slide)|| slide.lenght <= 0) {
-  return null;  
- }
-    return (
-        <section className="slider">
-            <FaArrowAltCircleLeft className="left-arrow" onClick={prevSlide} />
-            <FaArrowAltCircleRight className="right-arrow" onClick={nextSlide}/>
-            {SliderData.map((slide, index)=>{
-                    return(
-                        <div className={index === current ? 'slide-active' : 'slide'} key={index}>
-                           {index === current && (<img src={slide.image} alt="Road map rover" className='image'/>;)} 
-                            
-                        </div>
-                    )
-                        
-                })
-            }
-        </>    
-    )
+  const prevSlide = () => {
+    setCurrent(current === 0 ? length - 1 : current - 1);
+  };
+
+  if (!Array.isArray(slide) || slide.lenght <= 0) {
+    return null;
+  }
+  return (
+    <section className="slider">
+      <FaArrowAltCircleLeft className="left-arrow" onClick={prevSlide} />
+      <FaArrowAltCircleRight className="right-arrow" onClick={nextSlide} />
+      {SliderData.map((slide, index) => (
+        <div className={index === current ? 'slide-active' : 'slide'} key={index}>
+          {index === current && (<img src={slide.image} alt="Road map rover" className="image" />)}
+
+        </div>
+      ))}
+    </section>
+  );
 };
 
 export default ImageSlider;

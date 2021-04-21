8f7483c13aad31118c038795c8849882efe323e6
Nicolas-concina <nicoconcina@gmail.com>
2021-04-21 10:51:54 +0200

    RoadMap Create

---
+++ src/components/RoadMap/SliderData.js
@@ -0,0 +1,15 @@
+export const SliderData = [
+    {
+        image:'https://mars.nasa.gov/system/downloadable_items/45798_PIA24486-Drive-Map.png', //perseverance img
+
+    },
+    {
+        image:'https://d2pn8kiwq2w21t.cloudfront.net/original_images/jpegPIA23412.jpg', //curiosity img
+
+    },
+    {
+        image:'https://upload.wikimedia.org/wikipedia/commons/thumb/4/4e/Opportunity_rover_lifetime_progress_map.jpg/400px-Opportunity_rover_lifetime_progress_map.jpg', // opportunity img
+
+
+    },
+];

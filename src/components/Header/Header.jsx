6bb51931e77e1abe82bd3677d3e0315ccd8864e1
David <david-bourrel@hotmail.fr>
2021-04-29 13:42:01 +0200

    Mars 3D and content about Mars in the Home page, done!

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,6 +5,7 @@ const Header = () => (
     <header className="headerHomePage">
       <iframe
         src="https://solarsystem.nasa.gov/gltf_embed/2372"
+        className="mars3d"
         title="mars 3d"
         height="500"
         width="100%"
@@ -14,13 +15,14 @@ const Header = () => (
         backgroundImage="https://leblob.fr/sites/default/files/styles/full_width_big/public/2019-02/astro.jpg?h=1257b885&itok=T0ew8nOO"
       />
       <div className="headerButton">
-        <h1>Perseverance et son acolyte Ingenuity</h1>
+        <h1>What`s new about Perseverance and his friend Ingenuity</h1>
+
         <a
           href="https://www.cite-espace.com/actualites-spatiales/ingenuity-premier-survol-mars/"
           target="_blank"
           rel="noreferrer"
         >
-          Voir l`actu de Mars !
+          <button type="button">See more !</button>
         </a>
       </div>
     </header>
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -6,12 +6,25 @@ const Home = () => (
     <div className="container">
       <Header />
       <div className="homeMessage">
-        <h2>Explore mars !</h2>
+        <h2>Mars Exploration Rovers</h2>
         <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis
-          qui blanditiis a dolor illum laborum rem, dignissimos fuga, modi
-          laboriosam nulla dicta consequuntur hic? Quod cum ipsa non blanditiis
-          tempora!
+          The mission consists of deploying rovers on the Martian soil to study
+          its surface, take pictures and collect samples of the Martian soil.
+          ... The final goal is to be able to carry out a detailed analysis of
+          the Martian soil samples on Earth and in particular to identify
+          possible ancient life forms.
+        </p>
+        <p>
+          As of February 2021, there have been five successful robotically
+          operated Mars rovers, all managed by the Jet Propulsion Laboratory:
+          <em>Sojourner, Opportunity, Spirit, Curiosity, and Perseverance.</em>
+        </p>
+        <p>
+          On January 24, 2016, NASA reported that current studies on Mars by
+          Curiosity and Opportunity would be searching for evidence of ancient
+          life, including a biosphere based on autotrophic, chemotrophic or
+          chemolithoautotrophic microorganisms, as well as ancient water,
+          including fluvio-lacustrine environments that may have been habitable.
         </p>
       </div>
     </div>

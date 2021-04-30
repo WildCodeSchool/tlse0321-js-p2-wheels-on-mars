2a4bdbd9d586469f3cdc51fb23b65f3cb8fa2045
Sebastien Francois <sebastien.devlopweb@gmail.com>
2021-04-30 10:13:35 +0200

    Delete RoadMap.jsx

--- src/components/RoadMap/RoadMap.jsx
+++
@@ -1,9 +0,0 @@
-import './roadmap.css';
-import ImageSlider from './components/ImageSlider';
-import SliderData from './components/SliderData';
-
-function RoadMap() {
-  return <ImageSlider slides={SliderData} />;
-}
-
-export default RoadMap;

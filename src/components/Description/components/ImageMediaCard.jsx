b5175d05cc470fa4af221206cfeeb7bb5fd115fa
SebastienFrancois <sebastien.devlopweb@gmail.com>
2021-05-05 15:03:31 +0200

    Update size of json text for cam description and imgCar

--- src/components/Description/components/ImageMediaCard.jsx
+++ src/components/Description/components/ImageMediaCard.jsx
@@ -2,23 +2,21 @@ import { makeStyles, useTheme } from '@material-ui/core/styles';
 import { useMediaQuery } from '@material-ui/core';
 import Card from '@material-ui/core/Card';
 import CardActionArea from '@material-ui/core/CardActionArea';
-import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
-import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 
 const useStyles = makeStyles({
   card: {
-    height: '200px',
-    maxWidth: '60%',
+    height: '300px',
+    maxWidth: '80%',
     display: 'flex',
     margin: '2rem auto',
     color: '#fff',
   },
   cardResponsive: {
     height: 'auto',
-    maxWidth: '80%',
+    maxWidth: '60%',
     display: 'flex',
     flexDirection: 'column',
     margin: '2rem auto',
@@ -28,6 +26,7 @@ const useStyles = makeStyles({
     display: 'flex',
     flexDirection: 'column',
     justifyContent: 'center',
+    alignItems: 'center',
     textAlign: 'start',
     backgroundColor: '#13103C',
   },
@@ -45,8 +44,9 @@ export default function ImgMediaCard({ name, image, txt }) {
     <Card className={isMatch ? classes.cardResponsive : classes.card}>
       <CardMedia
         component="img"
-        alt={name}
+        width="50%"
         height="100%"
+        alt={name}
         image={image}
         title={name}
       />
@@ -59,11 +59,6 @@ export default function ImgMediaCard({ name, image, txt }) {
             {txt}
           </Typography>
         </CardContent>
-        <CardActions className={classes.cardButton}>
-          <Button size="small" style={{ color: '#fff' }}>
-            Learn More ...
-          </Button>
-        </CardActions>
       </CardActionArea>
     </Card>
   );

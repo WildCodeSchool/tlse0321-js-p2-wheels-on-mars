94346b752768161cf440be46daf113774bd70fcc
SebastienFrancois <sebastien.devlopweb@gmail.com>
2021-05-05 11:36:23 +0200

    change data depending on content for Science

--- src/components/Description/components/ScienceArticle.jsx
+++ src/components/Description/components/ScienceArticle.jsx
@@ -8,32 +8,6 @@ import {
 } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 import { useState } from 'react';
-import data from '../../../dto.json';
-
-const infos = data.rovers[0];
-const bullInfo = [
-  {
-    title: infos.curiosity.science.goals.goal1,
-    text:
-      'The rovers are designed to study the rock record to reveal more about the geologic processes that created and modified the Martian crust and surface through time. Each layer of rock on the Martian surface contains a record of the environment in which it was formed. The rover seeks evidence of rocks that formed in water and that preserve evidence of organics, the chemical building blocks of life.',
-  },
-  {
-    title: 'Climate Goal:',
-    text:
-      "Past Martian climate conditions are a focus of the Perseverance rover mission. The rover's instruments are looking for evidence of ancient habitable environments where microbial life could have existed in the past.",
-  },
-  {
-    title: 'Biology samples:',
-    text: `The rovers focuses on surface-based studies of the Martian environment, seeking preserved signs of biosignatures in rock samples that formed in ancient Martian environments with conditions that might have been favorable to microbial life.
-    It is the first rover mission designed to seek signs of past microbial life. Earlier rovers first focused on and confirmed that Mars once had habitable conditions.`,
-  },
-  {
-    title: 'Prepare human arrival on Mars:',
-    text: `The rovers are demonstrating key technologies for using natural resources in the Martian environment for life support and fuel. It is also monitoring environmental conditions so mission planners understand better how to protect future human explorers.
-    This science goal relates to national space policy for sending humans to Mars in the 2030s. Similar to the history of the exploration of Earth's moon, robotic missions to Mars provide a crucial understanding of the environment and test innovative technologies for future human exploration.
-    Investments in Mars 2020 technologies include contributions from NASA's Human Exploration and Operations (HEO) Mission Directorate and Space Technology Program (STP) as part of NASA's long-term efforts to develop future capabilities for human space exploration.`,
-  },
-];
 
 const useStyles = makeStyles((theme) => ({
   small: {
@@ -59,38 +33,38 @@ const useStyles = makeStyles((theme) => ({
     width: '100%',
   },
 }));
-const ScienceArticle = () => {
+
+const ScienceArticle = ({ science }) => {
+  const bullInfo = [
+    {
+      title: science.goals.goal1,
+      text: science.goals.text1,
+    },
+    {
+      title: science.goals.goal2,
+      text: science.goals.text2,
+    },
+    {
+      title: science.goals.goal3,
+      text: science.goals.text3,
+    },
+    {
+      title: science.goals.goal4,
+      text: science.goals.text4,
+    },
+  ];
+
   const [textValue, setTextValue] = useState(0);
   const classes = useStyles();
   return (
     <Paper elevation={0}>
       <Container>
         <Typography variant="body1" className={classes.paragraph}>
-          Perseverance is set to explore Jezero crater. From orbit, this crater
-          shows all the promising signs of a place that was likely friendly to
-          life in the distant past. The rover’s goal is to study the site in
-          detail for its past conditions and seek the very signs of past life.
-          Its mission is to identify and collect the most compelling rock core
-          and soil samples, which a future mission could retrieve and bring back
-          to Earth for more detailed study. Perseverance will also test
-          technologies needed for the future human and robotic exploration of
-          Mars.
+          {science.text1}
         </Typography>
-        <img
-          src={infos.curiosity.science.img}
-          alt="rover"
-          className={classes.img}
-        />
+        <img src={science.img} alt="rover" className={classes.img} />
         <Typography variant="body1" className={classes.paragraph}>
-          Perseverance is set to explore Jezero crater. From orbit, this crater
-          shows all the promising signs of a place that was likely friendly to
-          life in the distant past. The rover’s goal is to study the site in
-          detail for its past conditions and seek the very signs of past life.
-          Its mission is to identify and collect the most compelling rock core
-          and soil samples, which a future mission could retrieve and bring back
-          to Earth for more detailed study. Perseverance will also test
-          technologies needed for the future human and robotic exploration of
-          Mars.
+          {science.text2}
         </Typography>
         <Typography variant="h4" align="center">
           4 Goals
@@ -110,7 +84,7 @@ const ScienceArticle = () => {
               src="https://images.unsplash.com/photo-1521401661174-c6ef32c9dfea?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80"
             />
             <Typography variant="h5" align="center">
-              Geology
+              {science.goals.goal1}
             </Typography>
           </ButtonBase>
           <ButtonBase
@@ -127,7 +101,7 @@ const ScienceArticle = () => {
               src="https://images.unsplash.com/photo-1576158114131-f211996e9137?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80"
             />
             <Typography variant="h5" align="center">
-              Climate
+              {science.goals.goal2}
             </Typography>
           </ButtonBase>
           <ButtonBase
@@ -144,7 +118,7 @@ const ScienceArticle = () => {
               src="https://images.unsplash.com/photo-1576086213369-97a306d36557?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80"
             />
             <Typography variant="h5" align="center">
-              Biology
+              {science.goals.goal3}
             </Typography>
           </ButtonBase>
           <ButtonBase
@@ -161,7 +135,7 @@ const ScienceArticle = () => {
               src="https://images.unsplash.com/photo-1541873676-a18131494184?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=661&q=80"
             />
             <Typography variant="h5" align="center">
-              Human arrival
+              {science.goals.goal4}
             </Typography>
           </ButtonBase>
         </Box>
